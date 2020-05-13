716e384864a198a90e671b4f382f233a03d64b3d
UlyssesWerlich <werlichborgesulysses@gmail.com>
2020-05-13 15:25:34 -0300

    005 - Alterado o controller

--- alterarDados.php
+++ alterarDados.php
@@ -4,57 +4,56 @@
     include('cabecalho.php');
 
 
-$nome = $_POST['nome'];
-$endereco = $_POST['endereco'];
-$turma = $_POST['turma'];
-$dataNascimento = $_POST['dataNascimento'];
-$botao = $_POST['Botao'];
+	$nome = $_POST['nome'];
+	$endereco = $_POST['endereco'];
+	$turma = $_POST['turma'];
+	$dataNascimento = $_POST['dataNascimento'];
+	$botao = $_POST['Botao'];
 
-$dataArray = explode("/", $dataNascimento);
-$dataConvertida = $dataArray[2]."-".$dataArray[1]."-".$dataArray[0];
+	$dataArray = explode("/", $dataNascimento);
+	$dataConvertida = $dataArray[2]."-".$dataArray[1]."-".$dataArray[0];
 
-if ($botao == 'Cadastrar'){
-	$turno = $_POST['turno'];
-	try{
-		$pdo=new PDO("mysql:host=localhost;dbname=focus","root","password");
-	}catch(PDOException $e){
-		echo $e->getMessage();
+	if ($botao == 'Cadastrar'){
+		$turno = $_POST['turno'];
+		try{
+			$pdo=new PDO("mysql:host=localhost;dbname=focus","root","password");
+		}catch(PDOException $e){
+			echo $e->getMessage();
+		}
+		$inserir=$pdo->prepare("Insert into alunos(nome, endereco, turma, turno, dataNascimento) Values('$nome', '$endereco', '$turma', '$turno', '$dataConvertida');");
+		$inserir->execute();
+		$pdo = null;
+		echo "<p>Aluno adicionado com sucesso</p>";
+
+	} elseif ($botao == 'Alterar'){
+		$id_alunos = $_POST['id_alunos'];
+		$turno = $_POST['turno'];
+		try{
+			$pdo=new PDO("mysql:host=localhost;dbname=focus","root","password");
+		}catch(PDOException $e){
+			echo $e->getMessage();
+		}
+		$inserir=$pdo->prepare("update alunos set nome='$nome', endereco='$endereco', turma='$turma', turno='$turno', dataNascimento='$dataConvertida' where id_alunos = $id_alunos;");
+		$inserir->execute();
+		$pdo = null;
+		echo "<p>Dados de aluno alterado com sucesso</p>";
+
+	} elseif ($botao == 'Excluir'){
+		$id_alunos = $_POST['id_alunos'];
+		try {
+			$pdo = new PDO("mysql:host=localhost;dbname=focus","root","password");
+		} catch (PDOException $e){
+			echo $e->getMessage();
+		}
+		$excluir = $pdo->prepare("delete from alunos where id_alunos = '$id_alunos'");
+		$excluir->execute();
+		$pdo = null;
+		echo "<p>Aluno excluído com sucesso</p>";
 	}
-	$inserir=$pdo->prepare("Insert into alunos(nome, endereco, turma, turno, dataNascimento) Values('$nome', '$endereco', '$turma', '$turno', '$dataConvertida');");
-	$inserir->execute();
-	$pdo = null;
-	echo "<p>Aluno adicionado com sucesso</p>";
-
-} elseif ($botao == 'Alterar'){
-	$id_alunos = $_POST['id_alunos'];
-	$turno = $_POST['turno'];
-	try{
-		$pdo=new PDO("mysql:host=localhost;dbname=focus","root","password");
-	}catch(PDOException $e){
-		echo $e->getMessage();
+	
+	else {
+		echo "<p>Erro ao realizar operação, favor repetir</p>";
 	}
-	$inserir=$pdo->prepare("update alunos set nome='$nome', endereco='$endereco', turma='$turma', turno='$turno', dataNascimento='$dataConvertida' where id_alunos = $id_alunos;");
-	$inserir->execute();
-	$pdo = null;
-	echo "<p>Dados de aluno alterado com sucesso</p>";
-
-} else {
-	$id_alunos = $_POST['id_alunos'];
-	try {
-		$pdo = new PDO("mysql:host=localhost;dbname=focus","root","password");
-	} catch (PDOException $e){
-		echo $e->getMessage();
-	}
-	$excluir = $pdo->prepare("delete from alunos where id_alunos = '$id_alunos'");
-	$excluir->execute();
-	$pdo = null;
-	echo "<p>Aluno excluído com sucesso</p>";
-}
-
-
-	//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-
-
 ?>
 
 </body>
--- editarAluno.php
+++ editarAluno.php
@@ -1,14 +1,13 @@
 <?php
     $tituloCabecalho = "Alterar dados de aluno";
     include('cabecalho.php');
-//$id = $_POST['id_alunos'];
-$id_alunos = $_GET['id_alunos'];
+    $id_alunos = $_GET['id_alunos'];
 
-try {
-    $pdo =new PDO("mysql:host=localhost;dbname=focus","root", "password");
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
+    try {
+        $pdo =new PDO("mysql:host=localhost;dbname=focus","root", "password");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 
     $consulta = $pdo->prepare("select * from alunos where id_alunos like '$id_alunos'");
     $consulta->execute();
@@ -17,31 +16,30 @@ try {
     foreach ($resultado as $row){
         $dataArray = explode("-", $row['dataNascimento']);
         $dataConvertida = $dataArray[2]."/".$dataArray[1]."/".$dataArray[0];
-
-        echo "<div class='formulario'>
-        <form name='form' method='POST' action='alterarDados.php'>";
-        echo "<p>Id do aluno</p>
-                <input type='text' name='id_alunos' value='$row[id_alunos]' readonly></p>";
-        echo "<p>Nome<br/>
-                <input type='text' name='nome' value='$row[nome]'></p>  
-            <p>Endereço<br/>
-                <input type='text' name='endereco' value='$row[endereco]'></p>  
-            <p>Turma<br/>
-                <input type='text' name='turma' value='$row[turma]'></p> 
-            <p>Selecione o Turno:<br>
-                <input class='campo' name='turno' type='radio' id='turno' value='manha'>Manhã
-                <input class='campo' name='turno' type='radio' id='turno' value='tarde'>Tarde
-                <input class='campo' name='turno' type='radio' id='turno' value='noite'>Noite</p>  
-            <p>Data de nascimento:<br>
-                <input type='text' name='dataNascimento' size='10' maxlength='10' minlength='10' value='$dataConvertida'> </p>
-            </p>     
+?>
+        <div class='formulario'>
+            <form name='form' method='POST' action='alterarDados.php'>";
+                <p>Id do aluno</p>
+                    <input type='text' name='id_alunos' value='<?php echo $row[id_alunos] ?>' readonly></p>";
+                <p>Nome<br/>
+                    <input type='text' name='nome' value='$row[nome]'></p>  
+                <p>Endereço<br/>
+                    <input type='text' name='endereco' value='$row[endereco]'></p>  
+                <p>Turma<br/>
+                    <input type='text' name='turma' value='$row[turma]'></p> 
+                <p>Selecione o Turno:<br>
+                    <input class='campo' name='turno' type='radio' id='turno' value='manha'>Manhã
+                    <input class='campo' name='turno' type='radio' id='turno' value='tarde'>Tarde
+                    <input class='campo' name='turno' type='radio' id='turno' value='noite'>Noite</p>  
+                <p>Data de nascimento:<br>
+                    <input type='text' name='dataNascimento' size='10' maxlength='10' minlength='10' value='$dataConvertida'> </p>
+                </p>     
             <input type='submit' name='Botao' value='Alterar'>
             <input type='submit' name='Botao' value='Excluir'>
         </form>
     </div>";
-
+<?php
     }
-
 ?>
 
 </body>
